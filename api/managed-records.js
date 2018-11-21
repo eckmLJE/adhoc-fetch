a63cbcc9b5e7a216b3f08e9fa97ba55f393c0b13
eckmLJE <lucas.eckman@gmail.com>
2018-11-21 13:05:35 -0500

    passing some tests

--- api/managed-records.js
+++ api/managed-records.js
@@ -5,29 +5,18 @@ import URI from "urijs";
 window.path = "http://localhost:3000/records";
 
 // Your retrieve function plus any additional functions go here ...
-const retrieve = optionsObj => {
+
+const retrieve = (optionsObj = {}) => {
+  console.log(optionsObj);
   let url = constructURL(optionsObj);
-  const transformed = fetchRecords(url).then(json => transformJson(json));
-  //   const pageData = getPageNumber(optionsObj);
+  console.log(url);
+  const transformed = fetchRecords(url).then(json =>
+    transformJson(json, optionsObj)
+  );
   return transformed;
 };
 
-const constructURL = optionsObj => {
-  let url = URI("http://localhost:3000/records").search({
-    limit: 10,
-    offset: 0
-  });
-  return optionsObj ? applyOptions(url, optionsObj) : url;
-};
-
-const applyOptions = (url, optionsObj) => {
-  console.log(optionsObj);
-  if (optionsObj.page) {
-    url.page = optionsObj.page;
-    url.offset = (optionsObj.page - 1) * 10;
-  }
-  return url;
-};
+// Fetch + URL
 
 const fetchRecords = url => {
   return fetch(url)
@@ -35,10 +24,32 @@ const fetchRecords = url => {
     .catch(console.log);
 };
 
-const transformJson = json => {
+const constructURL = optionsObj => {
+  let url = URI("http://localhost:3000/records");
+  const searchOptions = getSearchOptions(url, optionsObj);
+  return url.search(searchOptions);
+};
+
+const getSearchOptions = (url, optionsObj) => {
+  const searchOptions = { limit: 10, offset: 0 };
+  if (!!optionsObj.page && !!(optionsObj.page > 1)) {
+    searchOptions.offset = (optionsObj.page - 1) * 10;
+  }
+  if (optionsObj.colors) {
+    searchOptions.color = optionsObj.colors;
+  }
+  return searchOptions;
+};
+
+// Transform Response
+
+const transformJson = (json, optionsObj) => {
   const ids = json.map(el => el.id);
   const reduced = reduceOpenItems(json);
+  const pageData = getPageData(optionsObj);
   return {
+    previousPage: pageData.previousPage,
+    nextPage: pageData.nextPage,
     ids,
     open: reduced.open,
     closedPrimaryCount: reduced.closedPrimaryCount
@@ -62,6 +73,18 @@ const reduceOpenItems = json => {
 const checkPrimary = color => {
   const primaryColors = ["red", "blue", "yellow"];
   return primaryColors.includes(color);
+};
+
+const getPageData = optionsObj => {
+  const pageData = {
+    previousPage: null,
+    nextPage: 2
+  };
+  if (optionsObj.page > 1) {
+    pageData.previousPage = optionsObj.page - 1;
+    pageData.nextPage = optionsObj.page + 1;
+  }
+  return pageData;
 };
 
 export default retrieve;
