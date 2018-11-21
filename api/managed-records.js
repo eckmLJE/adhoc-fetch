ca39f57bb105868b3dc5ea968813bf27dcb92acf
eckmLJE <lucas.eckman@gmail.com>
2018-11-21 12:06:03 -0500

    pre pagination

--- api/managed-records.js
+++ api/managed-records.js
@@ -7,7 +7,9 @@ window.path = "http://localhost:3000/records";
 // Your retrieve function plus any additional functions go here ...
 const retrieve = optionsObj => {
   let url = constructURL(optionsObj);
-  return fetchRecords(url).then(json => transformJson(json));
+  const transformed = fetchRecords(url).then(json => transformJson(json));
+  //   const pageData = getPageNumber(optionsObj);
+  return transformed;
 };
 
 const constructURL = optionsObj => {
@@ -34,12 +36,9 @@ const fetchRecords = url => {
 };
 
 const transformJson = json => {
-  let closedPrimaryCount = 0;
   const ids = json.map(el => el.id);
   const reduced = reduceOpenItems(json);
   return {
-    previousPage: null,
-    nextPage: 2,
     ids,
     open: reduced.open,
     closedPrimaryCount: reduced.closedPrimaryCount
@@ -51,12 +50,8 @@ const reduceOpenItems = json => {
   const open = json.reduce((filtered, item) => {
     const isPrimary = checkPrimary(item.color);
     if (item.disposition === "open") {
-      filtered.push({
-        id: item.id,
-        color: item.color,
-        disposition: item.disposition,
-        isPrimary: isPrimary
-      });
+      item.isPrimary = isPrimary;
+      filtered.push(item);
     }
     if (item.disposition === "closed" && isPrimary) closedPrimaryCount++;
     return filtered;
